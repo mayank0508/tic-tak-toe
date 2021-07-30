15c41dd797fb819e4200b5415e776a576174980c
mayank0508 <thakurmayank0508@gmail.com>
2021-07-31 00:53:22 +0530

    Squares added

--- src/Components/Board/Board.js
+++ src/Components/Board/Board.js
@@ -3,17 +3,11 @@ import Sqaure from '../Sqaure/Sqaure';
 import { BoardStyle } from './Board.styles';
 
 function Board({ square, onClick }) {
-  return (
-    <BoardStyle>
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
-      <Sqaure onClick={() => onClick('Dummy')} />
+  return ( //  here we are displaying a square with 9 boxes as it was declare in Game components
+    <BoardStyle>  
+      {square.map((square, i) => (
+        <Sqaure key={i} value={square} onClick={() => onClick(i)} /> // this has the imported Square function
+      ))}
     </BoardStyle>
   );
 }
--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from '../Board/Board';
-// import { calculateWinner } from '../helper';
+import { calculateWinner } from '../../helper';
 
+//Hooks
 const Game = () => {
+  const [board, setBoard] = useState(Array(9).fill(null)); // this means that there is a state which is a array but it is currently empty
+  // the way we destructure the arrays are using the ES6 format.
+  const [xIsNext, setxIsNext] = useState(true);
+  const winners = calculateWinner(board); // thsi const tells us that this particular move is a winning move not
+
   const handleClick = () => {};
 
   const jumpTo = () => {};
 
   const renderMove = () => {};
 
-  return <Board onClick={handleClick} />;
+  return <Board square={board} onClick={handleClick} />;
 };
 
 export default Game;
