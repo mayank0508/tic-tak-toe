33fda4c0f92bb7e88f66b499237b2c8eb390d911
mayank0508 <thakurmayank0508@gmail.com>
2021-07-31 13:25:47 +0530

    Winner added

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Board from '../Board/Board';
 import { calculateWinner } from '../../helper';
+import { Gamestyle } from './Game.styles';
 
 //Hooks
 const Game = () => {
@@ -9,13 +10,36 @@ const Game = () => {
   const [xIsNext, setxIsNext] = useState(true);
   const winners = calculateWinner(board); // thsi const tells us that this particular move is a winning move not
 
-  const handleClick = () => {};
+  const handleClick = i => {
+    const boardCopy = [...board]; //here we are copying the data of the board, but in every case
+    //its not the case as cant be applicable to nested arrays
+
+    // if user click on a occupied box or the user has won the game, then just return
+    if (winners || boardCopy[i]) return;
+    // put a x or a o in a paricular clicked square
+    boardCopy[i] = xIsNext ? 'X' : 'O'; //here what we are doing is that acces the array using the i and then to let add X or O.
+    setBoard(boardCopy); // here in the setboard we are passing the [...board] that we made a copy of
+    setxIsNext(!xIsNext); // and then here we put setxIsNext to false, ie its O now !!
+  };
 
   const jumpTo = () => {};
 
   const renderMove = () => {};
 
-  return <Board square={board} onClick={handleClick} />;
+  return (
+    <>
+      {' '}
+      {/* we use fragments when we dont want to render an element like div
+    and we make a fragment here beacuse only one element can be returned in your JSX
+    */}
+      <Board square={board} onClick={handleClick} />
+      <Gamestyle>
+        <p>{  }</p>{' '}
+        {/* here we are using the curly brackets beacuse in side the JSX we can only 
+        use the JS in side those brackets */}
+      </Gamestyle>
+    </>
+  );
 };
 
 export default Game;
---
+++ src/Components/Game/Game.styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Gamestyle = styled.div`
+width: 200px;
+margin: 20px auto;
+`;
